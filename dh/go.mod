a81b408fbd289e182533bb1f6056e5b52222e305
Dmitrii Nashtykov <dmitrii.nashtykov@netcracker.com>
2024-01-11 23:46:52 +0300

    Diffy Hellman / Working version      - Сделал Работающую версию алгоритма Диффи Хеллмана

---
+++ dh/go.mod
@@ -0,0 +1,3 @@
+module dh
+
+go 1.21.0
---
+++ dh/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func SafePrime(bits uint) *big.Int {
+	var p *big.Int
+	checks := int(math.Max(float64(bits)/16, 20))
+	for {
+		p, _ = rand.Prime(rand.Reader, int(bits)-1)
+		p.Add(p.Lsh(p, 1), big.NewInt(1))
+
+		if p.ProbablyPrime(checks) {
+			return p
+		}
+	}
+}
+
+func main() {
+	const numOfBits uint = 64
+
+	privateAlice, _ := rand.Int(rand.Reader, new(big.Int).SetInt64(math.MaxInt64))
+	privateBob, _ := rand.Int(rand.Reader, new(big.Int).SetInt64(math.MaxInt64))
+
+	g := SafePrime(numOfBits)
+	p := SafePrime(numOfBits)
+
+	printArray := make([]byte, math.Ceil(float64(numOfBits)/8.0))
+
+	privateAlice.FillBytes(printArray)
+	fmt.Println("Alice Private: ", hex.EncodeToString(printArray))
+
+	privateBob.FillBytes(printArray)
+	fmt.Println("Bob Private: ", hex.EncodeToString(printArray))
+
+	g.FillBytes(printArray)
+	fmt.Println("g: ", hex.EncodeToString(printArray))
+
+	p.FillBytes(printArray)
+	fmt.Println("p: ", hex.EncodeToString(printArray))
+
+	ag := new(big.Int).Exp(g, privateAlice, p)
+
+	ag.FillBytes(printArray)
+	fmt.Println("a^g mod p: ", hex.EncodeToString(printArray))
+
+	bg := new(big.Int).Exp(g, privateBob, p)
+
+	bg.FillBytes(printArray)
+	fmt.Println("b^g mod p: ", hex.EncodeToString(printArray))
+
+	key := new(big.Int).Exp(bg, privateAlice, p)
+
+	key.FillBytes(printArray)
+	fmt.Println("Alice's key: ", hex.EncodeToString(printArray))
+
+	key = new(big.Int).Exp(ag, privateBob, p)
+
+	key.FillBytes(printArray)
+	fmt.Println("Bob's key: ", hex.EncodeToString(printArray))
+}
